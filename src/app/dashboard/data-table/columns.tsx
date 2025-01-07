930c784b0554b0d5172c0f2e290de463e877d9f7
Michael Rondon <mrondon72@gmail.com>
2025-01-07 23:36:03 +0100

    45. Formato de celdas

--- src/app/dashboard/data-table/columns.tsx
+++ src/app/dashboard/data-table/columns.tsx
@@ -2,23 +2,45 @@
 
 import { ColumnDef } from '@tanstack/react-table';
 
+import { Badge } from '@/components/ui/badge';
+
 import { Payment } from '@/data/payments.data';
 
 export const columns: ColumnDef<Payment>[] = [
     {
+        accessorKey: "clientName",
+        header: "Client Name"
+    },
+    {
         accessorKey: "status",
+        cell: ({ row }) => {
+            const status = row.getValue("status") as string;
+            const variant = {
+                failed: "destructive",
+                pending: "secondary",
+                processing: "info",
+                success: "success"
+            }[status] ?? "default" as any;
+
+            return <Badge capitalize variant={ variant }>{ status }</Badge>
+        },
         header: "Status"
+    },
+    {
+        accessorKey: "amount",
+        cell: ({ row }) => {
+            const amount = parseFloat(row.getValue("amount"));
+            const formatted = new Intl.NumberFormat("en-US", {
+                currency: "USD",
+                style: "currency"
+            }).format(amount);
+
+            return <div className="text-right font-medium">{ formatted }</div>
+        },
+        header: () => <div className="text-right">Amount</div>
     },
     {
         accessorKey: "email",
         header: "Email"
-    },
-    {
-        accessorKey: "amount",
-        header: "Amount"
-    },
-    {
-        accessorKey: "clientName",
-        header: "Client Name"
     }
 ];
