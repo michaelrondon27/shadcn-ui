09fd295371c8328a72898bc5f533f0aa7b1e4a2e
Michael Rondon <mrondon72@gmail.com>
2025-01-07 23:55:52 +0100

    46. Acciones de fila

--- src/app/dashboard/data-table/columns.tsx
+++ src/app/dashboard/data-table/columns.tsx
@@ -1,8 +1,12 @@
 "use client";
 
+import { DotsHorizontalIcon } from '@radix-ui/react-icons';
 import { ColumnDef } from '@tanstack/react-table';
+import { toast } from 'sonner';
 
 import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
 
 import { Payment } from '@/data/payments.data';
 
@@ -42,5 +46,41 @@ export const columns: ColumnDef<Payment>[] = [
     {
         accessorKey: "email",
         header: "Email"
+    },
+    {
+        cell: ({ row }) => {
+            const payment = row.original;
+
+            return (
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button className="h-8 w-8 p-0" variant="ghost">
+                            <span className="sr-only">Open menu</span>
+
+                            <DotsHorizontalIcon className='h-4 w-4' />
+                        </Button>
+                    </DropdownMenuTrigger>
+
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+
+                        <DropdownMenuItem onClick={ () => {
+                            navigator.clipboard.writeText(payment.id);
+
+                            toast("Payment ID copied to clipboard", { position: "top-right" });
+                        } }>
+                            Copy payment ID
+                        </DropdownMenuItem>
+
+                        <DropdownMenuSeparator />
+
+                        <DropdownMenuItem>View customer</DropdownMenuItem>
+
+                        <DropdownMenuItem>View payment details</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            );
+        },
+        id: "actions"
     }
 ];
