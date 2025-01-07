f0850f5a19e280405a6542f4980abc6683b6ccfb
Michael Rondon <mrondon72@gmail.com>
2025-01-07 19:22:22 +0100

    28.Menu Bar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -16,7 +16,8 @@ const links = [
     { name: "combobox", href: "combobox" },
     { name: "command", href: "command" },
     { name: "context menu", href: "context-menu" },
-    { name: "dialog", href: "dialog" }
+    { name: "dialog", href: "dialog" },
+    { name: "menu bar", href: "menu-bar" }
 ];
 
 export default function DashboardLayout({
@@ -166,7 +167,7 @@ export default function DashboardLayout({
                         </div>
                     </main>
 
-                    <footer className="bg-white md:flex md:items-center md:justify-between shadow rounded-lg p-4 md:p-6 xl:p-8 my-6 mx-4">
+                    <footer className="bg-white md:flex md:items-center md:justify-between shadow rounded-lg p-4 md:p-6 xl:p-8 my-6 mx-4 print:hidden">
                         <ul className="flex items-center flex-wrap mb-6 md:mb-0">
                             <li>
                                 <a
@@ -288,7 +289,7 @@ export default function DashboardLayout({
                         </div>
                     </footer>
 
-                    <p className="text-center text-sm text-gray-500 my-10">
+                    <p className="text-center text-sm text-gray-500 my-10 print:hidden">
                         &copy; 2019-{new Date().getFullYear()}{" "}
 
                         <a href="#" className="hover:underline" target="_blank">
